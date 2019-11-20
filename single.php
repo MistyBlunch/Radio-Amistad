83189ad321e3ae7058589a90094a4dba18621f24
Alex Aragón <aragcar@gmail.com>
2019-11-19 22:23:57 -0500

    add single post

--- single.php
+++ single.php
@@ -2,6 +2,29 @@
   get_header(); 
 ?>
   
+<div class="row">
+  <div class="col-md-8">
+
+    <?php while(have_posts()) : the_post(); ?>
+
+      <?php the_title(); ?>
+
+      <?php the_date(); ?>
+
+      <?php the_excerpt(); ?>
+
+      <?php the_content(); ?>
+
+    <?php endwhile; ?>
+  </div>
+  <div class="col-md-4">
+  
+  <?php get_sidebar(); ?>
+  <!-- aca va el sidebar -->
+  
+  </div>
+</div>
+
 <?php 
   get_footer(); 
 ?>
